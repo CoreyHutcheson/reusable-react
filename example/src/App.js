6966e0dd4efd1c2b7ab21b2c463f5e599f5ff6b6
Corey <hutcheson.coreyJ@gmail.com>
2019-06-19 18:25:29 -0500

    export links from example/App.js

--- example/src/App.js
+++ example/src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 
 import { Box, Navbar } from "@corey_hutcheson/reusable-react";
 
-const App = () => {
-  const links = [
-    {
-      element: "about",
-      text: "About"
-    },
-    {
-      element: "projects",
-      text: "Projects"
-    },
-    {
-      element: "contact",
-      text: "Contact"
-    }
-  ];
+export const links = [
+  {
+    element: "about",
+    text: "About"
+  },
+  {
+    element: "projects",
+    text: "Projects"
+  },
+  {
+    element: "contact",
+    text: "Contact"
+  }
+];
 
+const App = () => {
   return (
     <div>
       <Navbar links={links} />
