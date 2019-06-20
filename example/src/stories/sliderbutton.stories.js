18d56ac2e5fec1080dd6c75cd2717cddab6527f1
Corey <hutcheson.coreyJ@gmail.com>
2019-06-19 22:08:24 -0500

    Deletes sliderbutton story

--- example/src/stories/sliderbutton.stories.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import { SliderButton } from "../components/slider-button/SliderButton.jsx";
-
-storiesOf("Slider Button", module).add("Default", () => (
-  <SliderButton handleChange={() => console.log("changed")} />
-));
